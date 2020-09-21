cd7991ed076c4bc7d00678baab21da18e5612474
Aziz Mohammadi <azizmohammadi289@gmail.com>
2020-09-21 23:54:23 +0530

    I have created the footer of index.php

--- index.php
+++ index.php
@@ -14,43 +14,46 @@
     <title>booksXchange</title>
   </head>
   <body>
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top py-3" id="navbar">
-        <div class="container">
-          <a href="index.php" class="navbar-brand">booksXchange</a>
-          <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <form action="" class="form-inline">
-              <input type="text" class="form-control mr-2" placeholder="Search Books">
-              <input type="submit" value="Search Books" class="btn btn-outline-light">
-            </form>
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a href="index.php" class="nav-link active">Home</a>
-              </li>
-              <li class="nav-item">
-                <a href="about.php" class="nav-link">About</a>
-              </li>
-              <li class="nav-item">
-                <a href="faq.php" class="nav-link">FAQ</a>
-              </li>
-              <li class="nav-item mr-5">
-                <a href="contact.php" class="nav-link">Contact</a>
-              </li>
-              <li class="nav-item">
-                <a href="dashboard.php" class="nav-link">
-                <i class="fas fa-sign-in-alt"></i> Login</a>
-              </li>
-              <li class="nav-item">
-                <a href="signup.php" class="nav-link">
-                  <i class="fas fa-user-plus"></i> Sign Up</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <!-- HEADER SECTION --> 
+      <header id="main-header">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top py-3 main-navbar" id="main-navbar">
+            <div class="container">
+            <a href="index.php" class="navbar-brand">booksXchange</a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <form action="" class="form-inline">
+                <input type="text" class="form-control mr-2" placeholder="Search Books">
+                <input type="submit" value="Search Books" class="btn btn-outline-light">
+                </form>
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link active">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="about.php" class="nav-link">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="faq.php" class="nav-link">FAQ</a>
+                </li>
+                <li class="nav-item mr-5">
+                    <a href="contact.php" class="nav-link">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a href="dashboard.php" class="nav-link">
+                    <i class="fas fa-sign-in-alt"></i> Login</a>
+                </li>
+                <li class="nav-item">
+                    <a href="signup.php" class="nav-link">
+                    <i class="fas fa-user-plus"></i> Sign Up</a>
+                </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+      </header>
       <!-- SHOWCASE SECTION -->
       <section id="showcase">
         <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -73,7 +76,7 @@
       </section>
 
       <!-- HOME ICONS SECTION -->
-      <section id="home-icons" class="py-5">
+      <section id="home-icons" class="py-5 text-center bg-light">
         <div class="container">
         <h1 class="text-center mb-4">Our Goals</h1>
         <hr>
@@ -99,7 +102,7 @@
       </section>
 
       <!-- LOOKING BOOK SECTION --> 
-      <section id="lookingbook" class="p-4 mb-4">
+      <section id="lookingbook" class="p-4">
         <div class="dark-overlay">
           <div class="row">
             <div class="col">
@@ -112,7 +115,7 @@
         </div>
       </section>  
       <!-- INFO SECTION -->
-      <section id="info" class="py-3">
+      <section id="info" class="bg-light py-3">
           <div class="container">
             <div class="row">
               <div class="col-md-6 align-self-center">
@@ -129,7 +132,7 @@
       </section>
      
       <!-- BEST BOOKS -->
-      <section id="books" class="p-4 mb-4">
+      <section id="books" class="p-4 mb-4 bg-light">
         <div class="container">
           <hr>
           <h1 class="text-center">Our Best Books</h1>
@@ -177,19 +180,37 @@
       <section id="subscription" class="py-4">
         <div class="container">
           <h1 class="text-center text-uppercase text-primary">Subscription</h1>
-          <div class="row mb-4 ml-5">
+          <div class="row mb-4">
             <div class="col">
                 <h2 class="border-bottom d-flex py-2">Monthly Price <span class="text-danger ml-auto">$99</span></h2>
                 <h2 class="border-bottom py-2">Unlimited Book Landing</h2>
                 <h2 class="border-bottom py-2">Unlimited Book Share</h2>
                 <h2 class="border-bottom py-2">Cancel Any Time</h2>
-                <a href="login.php" class="btn btn-outline-primary btn-lg btn-block">Continue</a>
+                <a href="login.php" class="mt-3 btn btn-outline-primary btn-lg btn-block">Continue</a>
             </div>
           </div>
         </div>
       </section> 
 
     <!-- COMMENT SECTION --> 
+
+
+    <!-- GET IN TOUCH SECTION --> 
+
+
+    <!-- FOOTER SECTION --> 
+
+    <footer id="main-footer" class="p-4">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-md-6 mx-auto">
+                    <p class="lead">
+                        Copyright &copy; <span id="year"></span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -198,6 +219,9 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js"></script>
     <script>
+        // Get Full Year
+        $('#year').text(new Date().getFullYear());
+
       // Lightbox Init
       $(document).on('click', '[data-toggle="lightbox"]', function (event) {
         event.preventDefault();
